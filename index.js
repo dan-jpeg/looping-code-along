57f9e20c78d7573ca674a78d4c6e597c0d18c8a8
Dan Crowley <dancr.wley@gmail.com>
2023-01-20 20:57:59 -0600

    looping lab complet

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let cards = [];
+
+function writeCards(cards, event) {
+    for (let i = 0; i < cards.length; i++) {
+   cards[i] = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`) ;
+    }
+    return cards;
+}
+
+writeCards(["father time", "sister fate", "lady love",], "actualization");
+
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n--);
+    }
+}    
+countDown(10);
