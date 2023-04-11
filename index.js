19a5c71ffd270858526c03794c363845e765967d
dan <dancr.wley@gmail.com>
2023-04-11 11:06:40 -0400

    red go green

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 let cards = [];
 
+// function writeCards(cards, event) {
+//   for (let i = 0; i < [cards].length; i++){
+//     cards[i] = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`) ;
+//     }
+//     return cards;
+
+// }
+
 function writeCards(cards, event) {
-    for (let i = 0; i < cards.length; i++) {
-   cards[i] = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`) ;
-    }
-    return cards;
+  for (let i = 0; i < cards.length; i++) {
+  cards[i] = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`) ;
+  }
+  return cards;
 }
 
 writeCards(["father time", "sister fate", "lady love",], "actualization");
 
-
 function countDown(n) {
   while (n >= 0) {
-    console.log(n--);
-    }
-}    
+    console.log(n--)
+  }
+};
+
 countDown(10);
